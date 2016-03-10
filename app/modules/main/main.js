dedf46bf4e6cb424df9dfeff3f0e258778fa6b3f
madzhup <madzhup@gmail.com>
2016-03-10 18:42:06 +0200

    Config angular interpolate tags for preventing conflicts with twig

--- app/modules/main/main.js
+++ app/modules/main/main.js
@@ -4,6 +4,11 @@ let module = angular.module('app', []);
 
 export default module;
 
+module.config(($interpolateProvider) => {
+  $interpolateProvider.startSymbol('{!');
+  $interpolateProvider.endSymbol('!}');
+});
+
 angular.element(document).ready(() => {
   angular.bootstrap(document, ['app']);
 });
